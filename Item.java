531cc7fb8a374354f707f8e617806aecfe9c8799
lpfalz <102377622+lpfalz@users.noreply.github.com>
2022-04-22 19:08:35 -0400

    Message (init push of Item.java)

--- Item.java
+++ Item.java
@@ -1,3 +1,36 @@
 public class Item {
-    
+    private String name;        
+    private String brand;
+    private double price;
+    private int quantity;
+    private String expireDate;
+    //Attributes of Item from group meeting
+
+    public Item(String n, String b, double p, int q, String e){
+        this.name = n;
+        this.brand = b;
+        this.price = p;
+        this.quantity = q;
+        this.expireDate = e;
+    }
+    //Constructor for Item given specifications
+
+    public void add(int n){
+        this.quantity = this.quantity + n;
+    }
+    //method to add to existing item quantity
+
+    public String checkExpire(){
+        return this.expireDate;
+    }
+    //method to check expiry date of item
+
+    public int checkQuantity(){
+        return this.quantity;
+    }
+    //check quantity of given item
+
+//I'm still not too sure what you guys wanted me to do so I outlined a constructor 
+//and some very basic methods to check or add to an Item object
+
 }
